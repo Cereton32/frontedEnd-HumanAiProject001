1f8f40c89cd783c2bc6757a0ead9230700c2b700
Abhishek Kumar <2021pcecsabhishek010@poornima.org>
2025-04-23 07:38:14 +0530

    update .env files

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,15 +1,14 @@
-
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC8eDuA63oquMTAp_Cf2DxF9RXC2cTmlp4",
-    authDomain: "humanaiproject-ab1d5.firebaseapp.com",
-    projectId: "humanaiproject-ab1d5",
-    storageBucket: "humanaiproject-ab1d5.firebasestorage.app",
-    messagingSenderId: "925316767904",
-    appId: "1:925316767904:web:1a9be37b8715f5b1f4e9c5",
-    measurementId: "G-7CT9Z5JSVQ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
